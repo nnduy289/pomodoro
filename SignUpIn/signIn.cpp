ff556bd38f77ee0c4ef329902fe5329e13d13db9
duynhathoang <hnd2k3@gmail.com>
2026-02-01 00:33:30 +0700

    update feature: set dailyGoal and streak counter, but store data in text file :))

--- SignUpIn/signIn.cpp
+++ SignUpIn/signIn.cpp
@@ -12,6 +12,7 @@ int getOptionFromUser(){
     string option;
     bool flagIsInt;
     do{
+        cout << "Enter valid number (number > 0): ";
         flagIsInt = true;
         getline(cin, option);
         stringstream ss;
@@ -27,30 +28,29 @@ int getOptionFromUser(){
         //if flag is true, continue check each alphabet in string, otherwise loop again
         if(flagIsInt){
             for(int i = 0; i < option.length(); i++){
-                if(option[i] < '1' || option[i] > '9'){
+                if(option[i] < '0' || option[i] > '9'){
                 cout << "Invalid option\n";
                 flagIsInt = false;
                 break;
                 }
             }
+            if(flagIsInt){
+                optionNumber = stoi(option);
+                if(optionNumber <= 0){
+                    flagIsInt = false;
+                }else return optionNumber;
+            }
         }
-    
-    
     }while(!flagIsInt);
-    optionNumber = stoi(option);
-    if(optionNumber >= 1){
-        return optionNumber;
-    }else{
-        getOptionFromUser();
-    }
 }
 
 void showMenu(){
     cout << "======================" << '\n';
     cout << "=====================" << '\n';
     cout << "1. Set your ToDoList" << '\n';
-    cout << "2. Set your timer" << '\n';
-    cout << "3. Exit" << '\n';
+    cout << "2. Set your daily Goal (time)" << '\n';
+    cout << "3. See your streak!" << '\n';
+    cout << "4. Exit" << '\n';
     cout << "======================" << '\n';
     cout << "Choose your option: ";
 }
@@ -58,11 +58,10 @@ void showMenu(){
 void showMenuTimer(){
     cout << "======================" << '\n';
     cout << "=========TIMER==========" << '\n';
-    cout << "1. Set time for flow" << '\n';
-    cout << "2. Set time for relax" << '\n';
-    cout << "3. Set your daily goal!\n";
-    cout << "4. Start YOUR POM ?" << '\n';
-    cout << "5. Exit" << '\n';
+    cout << "1. Set your Pomo" << '\n';
+    cout << "2. Set your daily goal time!\n";
+    cout << "3. Start YOUR POM ?" << '\n';
+    cout << "4. Exit" << '\n';
     cout << "======================" << '\n';
     cout << "Choose your option: ";
 }
@@ -83,8 +82,9 @@ class User{
     private:
         string      userName;
         string      passWord;
-        int         counterStreak;
-        int         time = 0;
+        int         counterStreak = 0;
+        int         timeFocus = 0;
+        int         timeRelax = 0;
         int         dailyTotalTime = 0;
         int         dailyGoalTime = 0;
     public:
@@ -100,6 +100,15 @@ class User{
         void setTimer();
         int getTimer();
         void startTimer();
+
+        void setDailyGoalTime();
+        int getDialyGoalTime();
+
+        void restoreStreakFromFile();
+        void saveStreakToFile();
+        void setStreak();
+        int getStreak();
+        void checkStreak();
 };
 
 void User::setUserName(string &userName){
@@ -181,15 +190,76 @@ bool signIn(string& username, string& password){
 }
 
 void User::setTimer(){
-    this->time = getOptionFromUser();
+    cout << "Time focus: ";
+    this->timeFocus = getOptionFromUser();
+    cout << "Time relax: ";
+    this->timeRelax = getOptionFromUser();
 }
 
 void User::startTimer(){
-    countDownTimer(this->time);
+    cout << "Focus!!!\n";
+    countDownTimer(this->timeFocus);
+    cout << "Relax~~~\n";
+    countDownTimer(this->timeRelax);
 }
 
 int User::getTimer(){
-    return this->time;
+    return this->timeFocus;
+}
+
+void User::setDailyGoalTime(){
+    this->dailyGoalTime = getOptionFromUser();
+}
+
+int User::getDialyGoalTime(){
+    return this->dailyGoalTime;
+}
+
+void User::checkStreak(){
+    restoreStreakFromFile();
+    this->dailyTotalTime += this->timeFocus;
+    if(this->dailyTotalTime >= this->dailyGoalTime && this->dailyGoalTime > 0){
+        this->counterStreak++;
+
+        cout << "Congratulations! You have achieved your daily goal time!\n";
+        cout << "Your current streak is: " << this->counterStreak << " days!\n";
+    }else{
+        cout << "You have not achieved your daily goal time yet. Keep trying!\n";
+        cout << "Your current streak remains: " << this->counterStreak << " days.\n";
+    }
+    saveStreakToFile();
+}
+int User::getStreak(){
+    restoreStreakFromFile();
+    return this->counterStreak;
+}
+
+void User::saveStreakToFile(){
+    ofstream myFile("streak.txt");
+    myFile << this->userName << " " << this->counterStreak << '\n';
+    myFile.close();
+}
+
+void User::restoreStreakFromFile(){
+    saveStreakToFile();
+    string myText;
+    ifstream myReadFile("streak.txt");
+    while(getline(myReadFile, myText)){
+        stringstream ss(myText);
+        string word;
+        ss >> word;
+        if(word == this->userName){
+            ss >> word;
+            this->counterStreak = stoi(word);
+            myReadFile.close();
+            return;
+        }
+    }
+    myReadFile.close();
+}
+
+void User::setStreak(){
+    this->counterStreak = getOptionFromUser();
 }
 
 void userTimer(User &pom){
@@ -199,22 +269,21 @@ void userTimer(User &pom){
         
         switch(option){
             case 1:
-                cout << "How many time do you want to set? Type below:\n";
                 pom.setTimer();
                 break;
             case 2:
+                cout << "Set your daily goal time (in minutes): ";
+                pom.setDailyGoalTime();
                 break;
             case 3:
-                break;
-            case 4:
                 if(pom.getTimer() <= 0){
-                    cout << "How many time do you want to set? Type below:\n";
                     pom.setTimer();
                 }
                 cout << "START~!!!\n";
                 pom.startTimer();
+                pom.checkStreak();
                 break;
-            case 5:
+            case 4:
                 return;
             default:
                 break;
@@ -261,6 +330,9 @@ void handleUserAfterSignIn(string &username, string &password){
             userTimer(pom);
             break;
         case 3:
+            cout << "Your current streak is: " << pom.getStreak() << " days!\n";
+            break;
+        case 4:
             return;
         default:
             cout << "Invalid option, type again\n";
